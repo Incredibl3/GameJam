513db81d574c99754333b453df62cfdc6fd245e3
Đinh Văn Tiến <tiendv.52@gmail.com>
2015-11-15 12:34:17 +0700

    + Back button behavior

--- src/Application.js
+++ src/Application.js
@@ -179,6 +179,7 @@ exports = Class(GC.Application, function(supr) {
 		if (GLOBAL.NATIVE) {
 			GLOBAL.NATIVE.onBackButton = bind(this, function() {
 				logger.log ("onBackButton clicked!!!");
+				GLOBAL.NATIVE.sendActivityToBack();
 			});
 		}
 	};
